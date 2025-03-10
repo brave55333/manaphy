fc1ff29037d4cd94b0daf7036cd55da695029057
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-03-10 20:57:53 +0900

    Add contract addresses and ABI for Logistics and Split contracts

---
+++ pkgs/frontend/config/address.ts
@@ -0,0 +1,7 @@
+// コントラクトアドレスの設定
+export const addresses = {
+  Logistics: "0x00352b04d406Bc3E867e4d9752CfCc46A7633C8D",
+  PullSplitFactory: "0xA99e56B62e2296Ff2530a52aDc828290442AEfae",
+  PushSplitFactory: "0xCa83040d979084102951a765E8910043146E82cA",
+  SplitsWarehouse: "0x1079EE1271C80E224A42a2f9bfD2cFD6BfFF10A4",
+};
