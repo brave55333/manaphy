7d248c7e7915e4ad91d04c841d4236b9c2d75806
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-03-13 23:59:57 +0900

    Add payment functionality to PaymentPage component with transaction handling

--- pkgs/frontend/app/buyer/dashboard/page.tsx
+++ pkgs/frontend/app/buyer/dashboard/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { parseEther } from "viem";
+import { useSendTransaction } from "wagmi";
 
+/**
+ * PaymentPage Component
+ * @param param0
+ * @returns
+ */
 export default function PaymentPage({ params }: { params: { id: string } }) {
+  const { data: hash, sendTransaction } = useSendTransaction();
+
+  /**
+   * wagmiの機能を利用して0.1ETH支払うメソッド
+   */
+  const handlePayment = async () => {
+    // Splits contractに支払う
+    sendTransaction({
+      to: "0x62b6b43fae40b2577def9ca3b3f0b9e6bd0b0f19",
+      value: parseEther("0.1"),
+    });
+  };
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">注文 {params.id} の支払い</h1>
@@ -28,7 +50,7 @@ export default function PaymentPage({ params }: { params: { id: string } }) {
               <Label htmlFor="shipping-address">配送先住所</Label>
               <Input id="shipping-address" placeholder="配送先住所を入力" />
             </div>
-            <Button type="submit">支払いを確定</Button>
+            <Button onClick={handlePayment}>支払いを確定</Button>
           </form>
         </CardContent>
       </Card>
